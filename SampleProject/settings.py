84fae9ee87c871cf9010072ab74e1aa9de9481f4
Sonal0908 <72793854+Sonal0908@users.noreply.github.com>
2021-01-12 16:47:51 +0000

    SQL Server Native Client 11.0

--- SampleProject/settings.py
+++ SampleProject/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
         'PASSWORD':'Tcsmfg@1234#',
         'PORT':config('PORT'),
         'OPTIONS': {            
-            'driver': 'ODBC Driver 13 for SQL Server',
+            'driver': 'SQL Server Native Client 11.0',
             'MARS_Connection': 'True',
             }
     }
